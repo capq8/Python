6903a9ed96786c502bb5765b1b7003847e488116
Quang <quangogster@gmail.com>
2017-01-24 15:27:08 -0800

    added .py and completed p.1 encryption

---
+++ HW1.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+
+
+
+def cryptDict(str1, str2):
+	#write your code here
+	d = {}
+	for i,c in enumerate(str1):
+		d[c] = str2[i]
+	return d
+	pass
+
+def decrypt(cdict,s):
+	#write your code here
+	for i,c in enumerate(s):
+		if (cdict.has_key(c)):
+			s = s.replace(c,cdict[c])
+	return s
+	pass
+
+def testDecrypt():
+	cdict = cryptDict("abc", "xyz")
+	revcdict = cryptDict("xyz", "abc")
+	tests = "Now I know my abc's"
+	answer = "Now I know my xyz's"
+	if(decrypt(cdict, tests) != answer):
+		return False
+	if (decrypt(revcdict, decrypt(cdict, tests)) != "Now I know mb abc's"):
+		return False
+	if (decrypt(cdict,'') != ''):
+		return False
+	if (decrypt(cryptDict('',''), 'abc') != 'abc'):
+		return False
+	return True
+
+print(testDecrypt())
+
+
+
+#print(decrypt(cryptDict("abc", "xyz"), "adz"))
