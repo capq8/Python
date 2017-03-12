f0161bcc75f148cc800a5ca075565630d6abd9df
Quang Nguyen <quangogster@users.noreply.github.com>
2017-03-11 21:17:14 -0800

    Update scheme.scm

--- scheme.scm
+++ scheme.scm
@@ -20,61 +20,69 @@
     ((<= sum (addup L)) (numbersToSum sum (remove-last L)))
     (else L)))
 
-
+;first violation return #f else #t
 (define (isSorted L) 
   (cond ((null? L) #t)
         ((eq? (length L) 1) #t)
         ((< (car L) (car (cdr L))) (isSorted(cdr L)))
         (else #f)))
 
-
-
-(define (mergelists L1 L2)        
+;compare two value, inserting the smaller one first 
+(define (mergeUnique2 L1 L2)        
    (cond ( (null? L1) L2)
          ( (null? L2) L1)
-         ( (eq? (car L1)(car L2)) (mergelists (cdr L1)L2) )
+         ( (eq? (car L1)(car L2)) (mergeUnique2 (cdr L1)L2) )
          ( (< (car L1)(car L2))
-              (cons (car L1) (mergelists (cdr L1)L2)))
+              (cons (car L1) (mergeUnique2 (cdr L1)L2)))
          (else
-              (cons (car L2) (mergelists L1 (cdr L2))))
-   )
-)
+              (cons (car L2) (mergeUnique2 L1 (cdr L2))))
+))
 
 (define (fold f base L) ( cond
 ((null? L) base)
-(else (f (car L) (fold f base (cdr L)))))
-)
+(else (f (car L) (fold f base (cdr L))))
+))
 
 
-(define (mergeUniqueN L) (fold mergelists '() L 
- ))
+;standard
+(define (mergeUniqueN L) (fold mergeUnique2 '() L 
+))
 
 
+; use map to call f on car item then cons recursively
 (define (matrixMap f L ) (cond
       ( (null? L) '())
       (else (cons (map f (car L)) (matrixMap f (cdr L))))
 ))
                          
-                         
-                         
-;(cons (map f (car L)) (matrixMap f (cdr L)))
-;(map  f  L)
-;(mergeUniqueN
-;(display(numbersToSum 30 '(5 4 6 10 4 2 1 5)))
+(define (oddlistmaker L) (cond
+      ((null? L) '())
+      ((odd? (car L)) (cons (car L) (oddlistmaker (cdr L))))
+      (else (oddlistmaker (cdr L)))
+))    
+    
+; get odd arr, add up, div by length                   
+(define (avgOdd L) (cond
+                  ((null? (oddlistmaker L)) '())
+                  (else (/(fold + 0 (oddlistmaker L)) (length (oddlistmaker L))))
+))
+
+;return 2 list on null, map head of list to head of end list                  
+(define (unzip L) (cond
+                    ((null? L) '(() ()))
+                    (else (map list (car L) (car (cdr L))))
+                    ))                  
+
+
+
+;test cases
 ;(deepSum '(1 (2 3 4) (5) 6 7 (8 9 10) 11))
 ;(isSorted '(1 3 6 9 10))
-
-;(mergelists '(1 2 3 4) '(3 4 5 6))
-
-;(fold mergelists '() '((1 2 3) (3 4 5) (5 6))) 
-
-;display(mergeUniqueN '((1 2 3) (3 4 5) (5 6)))
-
-
-
-(matrixMap (lambda (x) (+ 1 x)) '((0 1 2) (3 4 5)) ) 
-      
+;(mergeUnique2 '(1 2 3 4) '(3 4 5 6))
+;(mergeUniqueN '((1 2 3) (3 4 5) (5 6)))
+;(matrixMap (lambda (x) (+ 1 x)) '((0 1 2) (3 4 5)) ) 
 ;(matrixMap (lambda (x) (* x x)) '((1 2) (3 4)) ) 
-;(map cons ’(a b c) ’((1) (2) (3))) 
+;(avgOdd '(1 2 4 6))
+;(unzip '((1 2) (3 4) (5 6)))
 
 
